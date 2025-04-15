fe858de63cfab0bbd6641ba71f0777a8d6997f91
Giovanni <giovannicruzvinhedo@gmail.com>
2025-04-15 17:49:09 -0300

    feat: user can view and create a role, and editing it

--- src/app/vagas/page.tsx
+++ src/app/vagas/page.tsx
@@ -1,8 +1,33 @@
-export default function Vagas() {
-    return (
-      <div className="">
-        <h1>Vagas</h1>
-      </div>
-    )
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import JobItem from "@/components/cards/job-item"
+
+export const dynamic = "force-dynamic"
+
+async function fetchJobs() {
+  try {
+    const res = await fetch("https://apis.codante.io/api/job-board/jobs", {
+      next: { revalidate: 60 },
+    })
+
+    return res.json()
+  } catch (error) {
+    console.error(error)
+    throw new Error("Failed to fetch jobs")
   }
-  
+}
+
+export default async function Vagas() {
+  const jobsData = await fetchJobs()
+  const jobs = jobsData.data
+
+  return (
+    <main className="">
+      <h2 className="font-display mb-12 text-2xl font-bold">Todas as Vagas</h2>
+      <div className="space-y-8">
+        {jobs.map((job: any) => (
+          <JobItem key={job.id} job={job} />
+        ))}
+      </div>
+    </main>
+  )
+}
